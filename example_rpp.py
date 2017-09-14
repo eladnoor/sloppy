19eca6dd7ab0ab084f8f330b26c49984c0305422
eladnoor <noor@imsb.biol.ethz.ch>
2017-09-14 15:42:58 +0200

    adding the script used for the rPP cycle in the Cell paper

--- example_rpp.py
+++ example_rpp.py
@@ -3,6 +3,9 @@ from python.optknock import OptKnock
 import logging
 import os
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 CARBON_SOURCES = ['g6p',
                   'f6p',
@@ -32,8 +35,8 @@ if __name__ == "__main__":
     
     title = 'rPP'; target_reaction = 'RBC'; knockins = 'RBC,PRK'
     
-    df_fname = 'res/data_%s.csv' % title
-    if False:    
+    df_fname = 'res/data_%s_%d.csv' % (title, max_ko)
+    if not os.path.exists(df_fname):    
         df = OptKnock.analyze_kos(CARBON_SOURCES, SINGLE_KOS,
                                   target_reaction, knockins, max_ko)
         with open(df_fname, 'w') as fp:
@@ -42,5 +45,36 @@ if __name__ == "__main__":
         df = pd.DataFrame.from_csv(df_fname)
 
     # create a 2D heatmap of all double knockouts and their slopes
+    # in the same order as figure S3 (A) in Antonovsky et al. 2016:
+    # doi: 10.1016/j.cell.2016.05.064
     
+    df = df[df['carbon source'] != 'electrons']
     
+    kos = SINGLE_KOS[1:]
+    css = CARBON_SOURCES[0:9]
+    index_list = df.knockouts.str.split('|').apply(
+            lambda l: list(map(SINGLE_KOS.index, l))).tolist()
+    df['ko1_idx'], df['ko2_idx'] = zip(*index_list)
+    
+    # replace the ko1 = 0 of the single knockouts with the same value of ko2,
+    # so they will appear on the diagonal of the matrix
+    df.loc[df.ko1_idx == 0, 'ko1_idx'] = df[df.ko1_idx == 0].ko2_idx
+    df['cs_idx'] = df['carbon source'].apply(CARBON_SOURCES.index)
+    
+    df['row'] = (df.ko2_idx-1) * 3 + df.cs_idx // 3
+    df['col'] = (df.ko1_idx-1) * 3 + df.cs_idx % 3
+    data_mat = np.ones((len(kos)*3, len(kos)*3)) * -10
+    
+    for i, r in df.iterrows():
+        if r['yield'] < 1e-9:
+            data_mat[r['row'], r['col']] = -10
+        else:
+            data_mat[r['row'], r['col']] = r['slope']
+
+    fig, ax = plt.subplots(figsize=(10, 8))
+    labels = [kos[i//3] if i%3==1 else '' for i in range(len(kos)*3)]
+    sns.heatmap(data_mat, vmin=-10,
+                cmap=sns.dark_palette("palegreen"), ax=ax,
+                xticklabels=labels, yticklabels=labels)
+    #hm = HeatMap(data_mat, x='KO1', y='KO2', values='slope')
+    #show(hm)
